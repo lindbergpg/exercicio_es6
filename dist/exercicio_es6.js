cc15c038bbee5b1164aa6adfc7cd57805d177756
lindbergpg <Lindbergpg@gmail.com>
2023-09-12 21:54:27 -0300

    criacao da funcao

--- dist/exercicio_es6.js
+++ dist/exercicio_es6.js
@@ -1,3 +1,32 @@
 "use strict";
 
-console.log("Hello Babel!");
+var alunos = ["Pedro"];
+//const notas = [7, 6, 4, 9, 5, 8];
+
+var alunos2 = alunos.map(function (aluno) {
+  return {
+    nome: aluno,
+    nota: 5
+  };
+});
+alunos2.push({
+  nome: "Maria",
+  nota: 8
+});
+alunos2.push({
+  nome: "Jose",
+  nota: 7
+});
+alunos2.push({
+  nome: "Julia",
+  nota: 6
+});
+alunos2.push({
+  nome: "Joao",
+  nota: 4
+});
+console.log(alunos2);
+var alunosAcimaDaMedia = alunos2.filter(function (item) {
+  return item.nota >= 6;
+});
+console.log(alunosAcimaDaMedia);
--- src/exercicio_es6.js
+++ src/exercicio_es6.js
@@ -1,1 +1,36 @@
-console.log("Hello Babel!");
+const alunos = ["Pedro"];
+
+const alunos2 = alunos.map(function(aluno) {
+    return {
+        nome: aluno,
+        nota: 5
+    }
+})
+
+alunos2.push({
+    nome: "Maria",
+    nota: 8
+})
+
+alunos2.push({
+    nome: "Jose",
+    nota: 7
+})
+
+alunos2.push({
+    nome: "Julia",
+    nota: 6
+})
+
+alunos2.push({
+    nome: "Joao",
+    nota: 4
+})
+
+console.log(alunos2);
+
+const alunosAcimaDaMedia = alunos2.filter(function(item) {
+    return item.nota >= 6
+})
+
+console.log(alunosAcimaDaMedia);
